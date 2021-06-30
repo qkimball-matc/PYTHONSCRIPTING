b72afb7964c724079f01a18e85198268a7548b66
Quinn Kimball <qkimball@madisoncollege.edu>
2021-06-30 10:40:29 -0700

    READY FOR GRADING week2 - added files for week2 and better organized week1

---
+++ week2/scriptname.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import sys
+
+"""
+prints "Hello World" to the screen
+Usage: ./scriptname.py
+"""
+
+print("Hello, my name is Quinn Kimball")
+print("In this module I learned how to pass information using the sys.argv[] list.")
+print("For instance, the name of this script is:", sys.argv[0])
